6d66eed1db3b10042b5069c72b6ab0b26debb15f
RenataUzeda <renatauzedasilva@gmail.com>
2023-03-15 19:00:02 -0300

    Add Class Conteudo, Dev e Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,10 +22,36 @@ public class Main {
         mentoria.setDescricao("Mentoria Java para iniciante");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Conhecendo a linguagem Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev developer1 = new Dev();
+        developer1.setNome("Renata");
+        developer1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos " + developer1.getNome() + developer1.getConteudosInscritos());
+        developer1.progredir();
+        developer1.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos " + developer1.getNome() + developer1.getConteudosInscritos());
+        System.out.println("Conteudos concluidos " + developer1.getNome() + developer1.getConteudosConcluidos());
+        System.out.println("XP: " + developer1.calcularTotalXp());
 
+        System.out.println("---------------");
+
+        Dev developer2 = new Dev();
+        developer2.setNome("Artur");
+        developer2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos " + developer2.getNome() + developer2.getConteudosInscritos());
+        developer2.progredir();
+        developer2.progredir();
+        developer2.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos " + developer2.getNome() + developer2.getConteudosInscritos());
+        System.out.println("Conteudos concluidos " + developer2.getNome() + developer2.getConteudosConcluidos());
+        System.out.println("XP: " + developer2.calcularTotalXp());
     }
 }
